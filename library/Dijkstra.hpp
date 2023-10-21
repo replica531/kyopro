0b28578285b61ed871d9756e5bcc15a6730c5e13
Sei Fukushima <sftakkyu1283@gmail.com>
2023-10-21 22:43:02 +0900

    feat: library編集

--- library/Dijkstra.hpp
+++ library/Dijkstra.hpp
@@ -1,9 +1,11 @@
-const int INF = 7+(1e+9);
-typedef int Weight;
+using ll = long long;
+
+const ll INF = 2e15;
+typedef ll Weight;
 struct Edge {  //src:辺の始点,dst:辺の終点,weight:辺の重さ
-  int src, dst;
+  ll src, dst;
   Weight weight;
-  Edge(int src, int dst, Weight weight) :
+  Edge(ll src, ll dst, Weight weight) :
     src(src), dst(dst), weight(weight) { }
 };
 bool operator < (const Edge &e, const Edge &f) {
@@ -14,8 +16,8 @@ typedef vector<Edge> Edges;
 typedef vector<Edges> Graph;
 //引数 g:隣接リスト,s:始点,dist:各頂点までの距離が入る,prev:最短路木の親頂点が入る
 //戻値 なし
-void shortestPath(const Graph &g,int s,vector<Weight> &dist,vector<int> &prev) {
-  int n = g.size();
+void shortestPath(const Graph &g,ll s,vector<Weight> &dist,vector<ll> &prev) {
+  ll n = g.size();
   dist.assign(n, INF); dist[s] = 0;
   prev.assign(n, -1);
   priority_queue<Edge> Q;
@@ -35,9 +37,9 @@ void shortestPath(const Graph &g,int s,vector<Weight> &dist,vector<int> &prev) {
 
 //引数 prev:最短路木の親頂点集合,t:終点
 //戻値 path:sからtへの最短経路
-vector<int> buildPath(const vector<int> &prev, int t) {
-  vector<int> path;
-  for (int u = t; u
+vector<ll> buildPath(const vector<ll> &prev, ll t) {
+  vector<ll> path;
+  for (ll u = t; u
    >= 0; u = prev[u])
     path.push_back(u);
   reverse(path.begin(), path.end());
@@ -51,7 +53,7 @@ int main(){
   g[s].push_back(Edge(s,t,w));    //隣接リストgにsからtに向かう重さwの辺を追加
   // ...
   vector<Weight> dist;
-  vector<int> prev;
+  vector<ll> prev;
   shortestPath(g,0,dist,prev);
   //distの中身が始点からの最短距離になる
   // ...
