34d784293654543f59d36aef8fc8afba47bdad23
Sei Fukushima <sftakkyu1283@gmail.com>
2023-12-10 10:25:29 +0900

    fix

--- library/dfs.hpp
+++ library/dfs.hpp
@@ -1,14 +1,16 @@
-vector<vector<int>>
-    graph;  // graph[v]には頂点v につながっている頂点の集合が入っているとする
+using ll = long long;
+using vl = vector<ll>;
+using vll = vector<vl>;
+vll graph(n,vl(0));  // graph[v]には頂点v につながっている頂点の集合が入っているとする
 vector<bool> vec(n, false);
 
-void dfs(int v) {  // v に関して深さ優先探索(vは頂点の番号)
+void dfs(ll v) {  // v に関して深さ優先探索(vは頂点の番号)
   vec[v] = true;   // v に訪問済みの印を付ける
 
   /*   v を処理する   */
 
-  for (int i : graph[v]) {  // v に接続している各頂点i に対し
-    if (vec[i] == false) {  // i が未訪問なら
+  for (ll i : graph[v]) {  // v に接続している各頂点i に対し
+    if (!vec[i]) {  // i が未訪問なら
       dfs(i);               // i に関して深さ優先探索
     }
   }
